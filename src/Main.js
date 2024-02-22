41db3fc82fc7e2e1f35e4a21ad4d338278bdf2c4
Mobina.Ghasempour <romi.ghasem@gmail.com>
2024-02-22 20:10:17 +0330

    handleClick()optimized.product's names set in[].

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,15 @@
 import { products } from "./Data";
+import { useState } from "react";
 const Main = () => {
 
   return (
     <>
       <div className="container">
         {products.map(({ img, name, cost, id }) => (
-          <div className="product">
+          <div className="product" key={id}>
             <img src={img} style={{ width: '250px',height:'240px' }}></img>
             <h1>{name}</h1>
-            <h3>{cost} $</h3>
-            <button className="btns">select</button>
+            <h3>{cost} $</h3>  
           </div>
         ))}
       </div>
--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,3 +1,29 @@
+import { products } from "./Data";
+import { useState, useEffect } from "react";
 
-const ShoppingCart = () => { }
+const ShoppingCart = () => { 
+
+  const btns = document.querySelectorAll('.btns');
+  const [productName, setProductName] = useState([]);
+  // const [array, setArray] = useState([]);
+  const handleClick = (id) => {
+    if (id == 1) setProductName([...productName,products[0].name]);
+    if (id == 2) setProductName([...productName,products[1].name]);
+    console.log(productName)
+  }
+
+  // console.log(btns[0].__reactProps$jclumx9nnq9.onClick)
+  // btns[0].addEventListener('click', () => {
+  //   handleClick('h')
+  // })
+  return (
+    <>
+      <button className="btns" onClick={() => handleClick(1)}>select</button>
+      <button className="btns" onClick={() => handleClick(2)}>select</button>
+      <h1>shop</h1>
+      <h2>{`${[...productName]}`}</h2> 
+
+    </>
+  )
+}
 export default ShoppingCart;
