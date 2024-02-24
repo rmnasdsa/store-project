66d4a2055c17b6c2f8ee7de28d8668151691469c
Mobina.Ghasempour <romi.ghasem@gmail.com>
2024-02-24 06:13:45 +0330

    increasing of count products rendered.

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,6 +1,7 @@
 import { products } from "./Data";
 import { useState, useEffect } from "react";
 
+
 const ShoppingCart = () => { 
 
   const btns = document.querySelectorAll('.btns');
@@ -8,59 +9,123 @@ const ShoppingCart = () => {
   const [productImage, setProductImage] = useState();
   const [productCost, setProductCost] = useState([]);
   let [sum, setSum] = useState(0);
+  let [count, setCount] = useState([0,0,0,0,0]);
   const [status, setStatus] = useState(false);
+
+  const remove = (id) => {
+    if (id == 1) {
+      const filter = productName.filter((name) => {
+        // console.log(name[0])
+        return name !== products[0].name;
+      })
+      console.log(filter);
+      setProductName(filter);
+      // setProductName([...productName ,null, null])
+      const index = productName.findIndex((name) => {
+        return name == products[0].name;
+      });
+      console.log(index);
+    }
+    if (id == 2) {
+      const filter = productName.filter((name) => {
+        return name !== products[1].name;
+      })
+      setProductName(filter);
+    }
+    if (id == 3) {
+      const filter = productName.filter((name) => {
+        return name !== products[2].name;
+      })
+      setProductName(filter);
+    }
+    if (id == 4) {
+      const filter = productName.filter((name) => {
+        return name !== products[3].name;
+      })
+      setProductName(filter);
+    }
+    if (id == 5) {
+      const filter = productName.filter((name) => {
+        return name !== products[4].name;
+      })
+      setProductName(filter);
+    }
+  }
   const handleClick = (id) => {
     if (id == 1) {
-      setProductName([...productName, products[0].name]);
+      count[0]++;
+      setProductName([...productName, products[0].name, count[0]]);
       setProductImage([products[0].img]);
       setProductCost([...productCost, products[0].cost]);
       setStatus(true);
     }
     if (id == 2) {
-      setProductName([...productName, products[1].name]);
+      count[1]++;
+      setProductName([...productName, products[1].name, count[1]]);
       setProductImage([products[1].img]);
       setProductCost([...productCost, products[1].cost]);
       setStatus(true);
     }
     if (id == 3) {
-      setProductName([...productName, products[2].name]);
+      count[2]++;
+      setProductName([...productName, products[2].name, count[2]]);
       setProductImage([products[2].img]);
       setProductCost([...productCost, products[2].cost]);
       setStatus(true);
     }
     if (id == 4) {
-      setProductName([...productName, products[3].name]);
+      count[3]++;
+      setProductName([...productName, products[3].name, count[3]]);
       setProductImage([products[3].img]);
       setProductCost([...productCost, products[3].cost]);
       setStatus(true);
     }
     if (id == 5) {
-      setProductName([...productName, products[4].name]);
+      count[4]++;
+      setProductName([...productName, products[4].name, count[4]]);
       setProductImage([products[4].img]);
       setProductCost([...productCost, products[4].cost]);
       setStatus(true);
     }
     console.log(productName)
-    console.log(productCost)
+    // console.log(productCost)
   }
+
+
+  // for (let i = 0; i < productName.length; i++){
+  //   if (productName.includes(products[0].name)) {
+  //     count++;
+  //     console.log(count);
+  //   }
+  //   // if (count > 1) {
+  //   //   productName.pop();
+  //   // }
+  // }
+
 
   // console.log(btns[0].__reactProps$jclumx9nnq9.onClick)
   // btns[0].addEventListener('click', () => {
   //   handleClick('h')
   // })
   return (
-    <>
-      <h1>shop</h1>
-      <h3>please choose your favorite products below</h3>
+    <div className="shop">
+      <h1 className="same-changes">shop</h1>
+      <h3 className="same-changes">please choose your favorite products below</h3>
       <button className="products-btns" onClick={() => handleClick(1)}>duffel bag</button>
       <button className="products-btns" onClick={() => handleClick(2)}>work bag</button>
       <button className="products-btns" onClick={() => handleClick(3)}>baseball bag</button>
       <button className="products-btns" onClick={() => handleClick(4)}>travel bag</button>
-      <button className="products-btns" onClick={() => handleClick(5)}>women's bag</button>
-     
+      <button className="products-btns" onClick={() => handleClick(5)}>women's bag</button><br></br>
+      {products.map(({id}) => (
+        <>
+          <button className="change-items" style={{fontSize:"18px"}} >-</button>
+          <button className="change-items" style={{fontSize:"15px"}} onClick={() => remove(id)}>X</button>
+        </>
+      ))}
       {/* <img src={status ? productImage : ""} style={{width: '250px',height:'240px'}}></img> */}
-      {status ? <h1 style={{width:'200px'}}>{`${[...productName]}`}</h1> : null}
-      <p>{status ? `${productCost} $` : null}</p>
+      {status ? <h1 style={{ width: '300px' }} className="same-changes">{`${[...productName]}`} </h1> : null}
+      
+      <p className="same-changes">{status ? `${productCost} $` : null}</p>
       <button className="btns" onClick={() => {
         productCost.map((cost) => {
           sum += cost;
@@ -68,16 +133,8 @@ const ShoppingCart = () => {
           })
         // console.log(sum)
       }}>sum</button>
-      <h4>{ sum} $</h4>
-      {/* {products.map(() => (
-        <> 
-          
-        </>
-      )
-       
-      )} */}
-      
-    </>
+      <h4 className="same-changes">{ sum} $</h4>
+    </div>
   )
 
   setStatus(false)
