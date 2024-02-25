0ab84c258303b415055e853b8fe24a97230cb7ad
Mobina.Ghasempour <romi.ghasem@gmail.com>
2024-02-25 12:10:01 +0330

    style changed.

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -15,48 +15,60 @@ const ShoppingCart = () => {
   const remove = (id) => {
     if (id == 1) {
 
-      const find = productName.find((name) => {
-        return name == count[0];
-      })
-      console.log(find);
+      // const find = productName.find((name) => {
+      //   return name == count[0];
+      // })
+      // // console.log(find);
+      // const index = productName.findIndex((name) => {
+      //   return name == products[0].name;
+      // });
+      // console.log(index);
+      const indexOf = productName.indexOf(count[0]);
+      console.log(indexOf);
       const filter = productName.filter((name) => {
         // console.log(name[0]);
         // const counter = productName.includes(count[0]);
         // console.log(counter);
         count[0] = 0;
-        return (name !== products[0].name && name !== find);
+        return (name !== products[0].name && name !== productName[indexOf]);
+        // && name !== productName[index + 1]
       })
-      console.log(filter);
+      // console.log(filter);
       setProductName(filter);
       // setProductName([...productName ,null, null]);
-      const index = productName.findIndex((name) => {
-        return name == products[0].name;
-      });
-      console.log(index);
+
      
      
     }
     if (id == 2) {
+      const indexOf = productName.indexOf(count[1]);
       const filter = productName.filter((name) => {
-        return name !== products[1].name;
+        count[1] = 0;
+        return (name !== products[1].name && name !== productName[indexOf]);
       })
       setProductName(filter);
     }
     if (id == 3) {
+      const indexOf = productName.indexOf(count[2]);
       const filter = productName.filter((name) => {
-        return name !== products[2].name;
+        count[2] = 0;
+        return (name !== products[2].name && name !== productName[indexOf]);
       })
       setProductName(filter);
     }
     if (id == 4) {
+      const indexOf = productName.indexOf(count[3]);
       const filter = productName.filter((name) => {
-        return name !== products[3].name;
+        count[3] = 0;
+        return (name !== products[3].name && name !== productName[indexOf]);
       })
       setProductName(filter);
     }
     if (id == 5) {
+      const indexOf = productName.indexOf(count[4]);
       const filter = productName.filter((name) => {
-        return name !== products[4].name;
+        count[4] = 0;
+        return (name !== products[4].name && name !== productName[indexOf]);
       })
       setProductName(filter);
     }
@@ -115,9 +127,9 @@ const ShoppingCart = () => {
       count[0]++;
       if (count[0] == 0) {
         count[0]++;
-        setProductName([...productName, products[0].name, count[0]]);
+        setProductName([ products[0].name, count[0]]);
       }
-      setProductName([...productName, products[0].name, count[0]]);
+      setProductName([ products[0].name, count[0]]);
       setProductImage([products[0].img]);
       setProductCost([...productCost, products[0].cost]);
       setStatus(true);
@@ -126,9 +138,9 @@ const ShoppingCart = () => {
       count[1]++;
       if (count[1] == 0) {
         count[1]++;
-        setProductName([...productName, products[1].name, count[1]]);
+        setProductName([ products[1].name, count[1]]);
       }
-      setProductName([...productName, products[1].name, count[1]]);
+      setProductName([ products[1].name, count[1]]);
       setProductImage([products[1].img]);
       setProductCost([...productCost, products[1].cost]);
       setStatus(true);
@@ -137,9 +149,9 @@ const ShoppingCart = () => {
       count[2]++;
       if (count[2] == 0) {
         count[2]++;
-        setProductName([...productName, products[2].name, count[2]]);
+        setProductName([ products[2].name, count[2]]);
       }
-      setProductName([...productName, products[2].name, count[2]]);
+      setProductName([ products[2].name, count[2]]);
       setProductImage([products[2].img]);
       setProductCost([...productCost, products[2].cost]);
       setStatus(true);
@@ -148,9 +160,9 @@ const ShoppingCart = () => {
       count[3]++;
       if (count[3] == 0) {
         count[3]++;
-        setProductName([...productName, products[3].name, count[3]]);
+        setProductName([ products[3].name, count[3]]);
       }
-      setProductName([...productName, products[3].name, count[3]]);
+      setProductName([ products[3].name, count[3]]);
       setProductImage([products[3].img]);
       setProductCost([...productCost, products[3].cost]);
       setStatus(true);
@@ -159,9 +171,9 @@ const ShoppingCart = () => {
       count[4]++;
       if (count[4] == 0) {
         count[4]++;
-        setProductName([...productName, products[4].name, count[4]]);
+        setProductName([ products[4].name, count[4]]);
       }
-      setProductName([...productName, products[4].name, count[4]]);
+      setProductName([ products[4].name, count[4]]);
       setProductImage([products[4].img]);
       setProductCost([...productCost, products[4].cost]);
       setStatus(true);
@@ -187,8 +199,8 @@ const ShoppingCart = () => {
   // })
   return (
     <div className="shop">
-      <h1 className="same-changes">shop</h1>
-      <h3 className="same-changes">please choose your favorite products below</h3>
+      <h1 className="same-changes header-width">shop</h1>
+      <h3 className="same-changes guide-text">please choose your favorite products below</h3>
       <button className="products-btns" onClick={() => handleClick(1)}>duffel bag</button>
       <button className="products-btns" onClick={() => handleClick(2)}>work bag</button>
       <button className="products-btns" onClick={() => handleClick(3)}>baseball bag</button>
@@ -203,7 +215,7 @@ const ShoppingCart = () => {
       {/* <img src={status ? productImage : ""} style={{width: '250px',height:'240px'}}></img> */}
       {status ? <h1 style={{ width: '300px' }} className="same-changes">{`${productName}`} </h1> : null}
       
-      <p className="same-changes">{status ? `${productCost} $` : null}</p>
+      <p className="same-changes cost-width">{status ? `${productCost} $` : null}</p>
       <button className="btns" onClick={() => {
         productCost.map((cost) => {
           sum += cost;
@@ -211,7 +223,7 @@ const ShoppingCart = () => {
           })
         // console.log(sum)
       }}>sum</button>
-      <h4 className="same-changes">{ sum} $</h4>
+      <h4 className="same-changes payments">{ sum} $</h4>
     </div>
   )
 
